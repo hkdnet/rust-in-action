caf885acc49f14746042e9ded8a950a11ed3e937
hkdnet <satoko.itse@gmail.com>
2019-05-20 21:07:40 +0900

    Impl sub_sort and compare_and_swap

--- src/bitonic.rs
+++ src/bitonic.rs
@@ -8,9 +8,19 @@ pub fn sort(xs: &mut [u32], up: bool) {
 }
 
 fn sub_sort(xs: &mut [u32], up: bool) {
-    unimplemented!()
+    if xs.len() > 1 {
+        compare_and_swap(xs, up);
+        let mid_point = xs.len() / 2;
+        sub_sort(&mut xs[..mid_point], up);
+        sub_sort(&mut xs[mid_point..], up)
+    }
 }
 
-fn compare_and_swap(xs: &[u32], up: bool) {
-    unimplemented!()
+fn compare_and_swap(xs: &mut [u32], up: bool) {
+    let mid_point = xs.len() / 2;
+    for i in 0..mid_point {
+        if (xs[i] > xs[mid_point + i]) == up {
+            xs.swap(i, mid_point + i)
+        }
+    }
 }
