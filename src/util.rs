79d5546b6a406eb9ea9c39d00f692d531d3b9981
hkdnet <satoko.itse@gmail.com>
2019-05-21 03:58:20 +0900

    Remove an unnecessary declaration

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,6 @@ use crate::bitonic::SortOrder;
 use rand::distributions::Standard;
 use rand::{Rng, SeedableRng};
 use rand_pcg::Pcg64Mcg;
-use std::cmp::Ordering;
 
 pub fn new_u32_vec(n: usize) -> Vec<u32> {
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
